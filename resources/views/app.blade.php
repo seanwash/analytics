d5e291d30eedbc69bff3139b0f1bcc6c1d7d597f
Sean Washington <hello@seanwash.com>
2023-02-07 22:24:40 -0800

    Use app.name env var for app title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Analytics</title>
+        <title>{{ config('app.name') }}</title>
 
         @vite(['resources/js/app.ts', 'resources/css/app.css'])
 
